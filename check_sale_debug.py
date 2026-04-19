88369207a065ddda56eac06973d8508b8835f239
Cristianv222 <cris.vasq2003@hotmail.com>
2026-04-19 15:57:56 -0500

    tu mensaje de commit

--- check_sale_debug.py
+++ check_sale_debug.py
@@ -5,7 +5,7 @@ import sys
 # Añadir el directorio actual al path
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventario.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vpmotos.settings')
 django.setup()
 
 from ventas.models import Venta, DetalleVenta
